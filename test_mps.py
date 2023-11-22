81131f5bc8145af1a0a76a5bae5c0114a1956343
Matt Parrett <matt.parrett@gmail.com>
2023-11-21 18:22:21 -0800

    Fixup

--- test_mps.py
+++ test_mps.py
@@ -144,19 +144,36 @@ sampler = DiffusionSampler(
     sigma_schedule=KarrasSchedule(sigma_min=0.0001, sigma_max=3.0, rho=9.0), # empirical parameters
     clamp=False
 )
-
-def inference(text, ref_s, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1, use_fp16=True):
+def tokenize(text, gpu_device):
     start = time.monotonic()
-
     text = text.strip()
     ps = global_phonemizer.phonemize([text])
     ps = word_tokenize(ps[0])
     ps = ' '.join(ps)
+    ps = ps.replace('``', '"')
+    ps = ps.replace("''", '"')
+
     tokens = textclenaer(ps)
     tokens.insert(0, 0)
-    tokens = torch.LongTensor(tokens).unsqueeze(0)  #.to(gpu_device)
+    #tokens = torch.LongTensor(tokens).to(gpu_device).unsqueeze(0)
+    tokens = torch.LongTensor(tokens).unsqueeze(0)  #.to(gpu_device) # because doesn't run on MPS
     duration = 1000*(time.monotonic() - start)
     print(f"Duration tokenize = {duration:.2f}ms")
+    return tokens
+
+
+def get_en(d_in, pred_aln_trg, dtype):
+    en = (d_in @ pred_aln_trg.unsqueeze(0).to(device=gpu_device, dtype=dtype))
+    if model_params.decoder.type == "hifigan":
+        asr_new = torch.zeros_like(en)
+        asr_new[:, :, 0] = en[:, :, 0]
+        asr_new[:, :, 1:] = en[:, :, 0:-1]
+        en = asr_new
+    return en
+
+
+def inference(text, ref_s, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1, use_fp16=True):
+    tokens = tokenize(text, gpu_device)
     
     with torch.no_grad():
         input_lengths = torch.Tensor([tokens.shape[-1]]).to(dtype=torch.int64) #.to(gpu_device)
@@ -201,22 +218,9 @@ def inference(text, ref_s, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding
             c_frame += int(pred_dur[i].data)
 
         # encode prosody
-        en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(gpu_device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(en)
-            asr_new[:, :, 0] = en[:, :, 0]
-            asr_new[:, :, 1:] = en[:, :, 0:-1]
-            en = asr_new
-
-        # what is F0Ntrain?
+        en = get_en(d.transpose(-1, -2), pred_aln_trg, torch.float32)
         F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-        
-        asr = (t_en @ pred_aln_trg.unsqueeze(0).to(device=gpu_device, dtype=USE_DTYPE))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(asr)
-            asr_new[:, :, 0] = asr[:, :, 0]
-            asr_new[:, :, 1:] = asr[:, :, 0:-1]
-            asr = asr_new
+        asr = get_en(t_en, pred_aln_trg, USE_DTYPE)
 
         start = time.monotonic()
         out = model.decoder(asr,
@@ -227,17 +231,9 @@ def inference(text, ref_s, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding
         print(f"Duration decoder = {duration:.2f}ms")
     return out.type(torch.float32).squeeze().cpu().numpy()[..., :-50] # weird pulse at the end of the model, need to be fixed later
 
-def LFinference(text, s_prev, ref_s, alpha = 0.3, beta = 0.7, t = 0.7, diffusion_steps=5, embedding_scale=1):
-  text = text.strip()
-  ps = global_phonemizer.phonemize([text])
-  ps = word_tokenize(ps[0])
-  ps = ' '.join(ps)
-  ps = ps.replace('``', '"')
-  ps = ps.replace("''", '"')
 
-  tokens = textclenaer(ps)
-  tokens.insert(0, 0)
-  tokens = torch.LongTensor(tokens).to(gpu_device).unsqueeze(0)
+def LFinference(text, s_prev, ref_s, alpha = 0.3, beta = 0.7, t = 0.7, diffusion_steps=5, embedding_scale=1):
+  tokens = tokenize(text, gpu_device)
 
   with torch.no_grad():
       input_lengths = torch.LongTensor([tokens.shape[-1]]).to(gpu_device)
@@ -247,33 +243,31 @@ def LFinference(text, s_prev, ref_s, alpha = 0.3, beta = 0.7, t = 0.7, diffusion
       bert_dur = model.bert(tokens, attention_mask=(~text_mask).int())
       d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
 
-      s_pred = sampler(noise = torch.randn((1, 256)).unsqueeze(1).to(gpu_device),
-                                        embedding=bert_dur,
-                                        embedding_scale=embedding_scale,
-                                          features=ref_s, # reference from the same speaker as the embedding
-                                            num_steps=diffusion_steps).squeeze(1)
+      noise = torch.randn((1, 256)).unsqueeze(1).to(gpu_device)
+      s_pred = sampler(
+        noise = noise,
+        embedding=bert_dur,
+        embedding_scale=embedding_scale,
+        features=ref_s, # reference from the same speaker as the embedding
+        num_steps=diffusion_steps).squeeze(1)
 
       if s_prev is not None:
           # convex combination of previous and current style
           s_pred = t * s_prev + (1 - t) * s_pred
 
-      s = s_pred[:, 128:]
-      ref = s_pred[:, :128]
-
+      s, ref = s_pred[:, 128:], s_pred[:, :128]
       ref = alpha * ref + (1 - alpha)  * ref_s[:, :128]
       s = beta * s + (1 - beta)  * ref_s[:, 128:]
 
       s_pred = torch.cat([ref, s], dim=-1)
 
-      d = model.predictor.text_encoder(d_en,
-                                        s, input_lengths, text_mask)
+      d = model.predictor.text_encoder(d_en, s, input_lengths, text_mask)
 
       x, _ = model.predictor.lstm(d)
       duration = model.predictor.duration_proj(x)
 
       duration = torch.sigmoid(duration).sum(axis=-1)
       pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
 
       pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
       c_frame = 0
@@ -282,29 +276,15 @@ def LFinference(text, s_prev, ref_s, alpha = 0.3, beta = 0.7, t = 0.7, diffusion
           c_frame += int(pred_dur[i].data)
 
       # encode prosody
-      en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(gpu_device))
-      if model_params.decoder.type == "hifigan":
-          asr_new = torch.zeros_like(en)
-          asr_new[:, :, 0] = en[:, :, 0]
-          asr_new[:, :, 1:] = en[:, :, 0:-1]
-          en = asr_new
-
+      en = get_en(d.transpose(-1, -2), pred_aln_trg, torch.float32)
       F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-
+      en = get_en(t_en, pred_aln_trg, USE_DTYPE)
       asr = (t_en @ pred_aln_trg.unsqueeze(0).to(gpu_device))
-      if model_params.decoder.type == "hifigan":
-          asr_new = torch.zeros_like(asr)
-          asr_new[:, :, 0] = asr[:, :, 0]
-          asr_new[:, :, 1:] = asr[:, :, 0:-1]
-          asr = asr_new
-
-      out = model.decoder(asr,
-                              F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-
+      out = model.decoder(asr, F0_pred, N_pred, ref.squeeze().unsqueeze(0))
   return out.squeeze().cpu().numpy()[..., :-100], s_pred # weird pulse at the end of the model, need to be fixed later
 
-def STinference(text, ref_s, ref_text, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1):
+
+def tokenize_st(text, gpu_device):
     text = text.strip()
     ps = global_phonemizer.phonemize([text])
     ps = word_tokenize(ps[0])
@@ -313,16 +293,12 @@ def STinference(text, ref_s, ref_text, alpha = 0.3, beta = 0.7, diffusion_steps=
     tokens = textclenaer(ps)
     tokens.insert(0, 0)
     tokens = torch.LongTensor(tokens).to(gpu_device).unsqueeze(0)
+    return tokens
 
-    ref_text = ref_text.strip()
-    ps = global_phonemizer.phonemize([ref_text])
-    ps = word_tokenize(ps[0])
-    ps = ' '.join(ps)
 
-    ref_tokens = textclenaer(ps)
-    ref_tokens.insert(0, 0)
-    ref_tokens = torch.LongTensor(ref_tokens).to(gpu_device).unsqueeze(0)
-
+def STinference(text, ref_s, ref_text, alpha = 0.3, beta = 0.7, diffusion_steps=5, embedding_scale=1):
+    tokens = tokenize_st(text, gpu_device)
+    ref_tokens = tokenize_st(ref_text, gpu_device)
 
     with torch.no_grad():
         input_lengths = torch.LongTensor([tokens.shape[-1]]).to(gpu_device)
@@ -333,58 +309,39 @@ def STinference(text, ref_s, ref_text, alpha = 0.3, beta = 0.7, diffusion_steps=
         d_en = model.bert_encoder(bert_dur).transpose(-1, -2)
 
         ref_input_lengths = torch.LongTensor([ref_tokens.shape[-1]]).to(gpu_device)
-        ref_text_mask = length_to_mask(ref_input_lengths).to(gpu_device)
-        ref_bert_dur = model.bert(ref_tokens, attention_mask=(~ref_text_mask).int())
-        s_pred = sampler(noise = torch.randn((1, 256)).unsqueeze(1).to(gpu_device),
-                                          embedding=bert_dur,
-                                          embedding_scale=embedding_scale,
-                                            features=ref_s, # reference from the same speaker as the embedding
-                                             num_steps=diffusion_steps).squeeze(1)
+        #ref_text_mask = length_to_mask(ref_input_lengths).to(gpu_device)
+        #ref_bert_dur = model.bert(ref_tokens, attention_mask=(~ref_text_mask).int())
+        noise = torch.randn((1, 256)).unsqueeze(1).to(gpu_device)
+        s_pred = sampler(
+            noise=noise,
+            embedding=bert_dur,
+            embedding_scale=embedding_scale,
+            features=ref_s, # reference from the same speaker as the embedding
+            num_steps=diffusion_steps).squeeze(1)
 
-
-        s = s_pred[:, 128:]
-        ref = s_pred[:, :128]
-
+        s, ref = s_pred[:, 128:], s_pred[:, :128]
         ref = alpha * ref + (1 - alpha)  * ref_s[:, :128]
         s = beta * s + (1 - beta)  * ref_s[:, 128:]
 
-        d = model.predictor.text_encoder(d_en,
-                                         s, input_lengths, text_mask)
+        d = model.predictor.text_encoder(d_en, s, input_lengths, text_mask)
 
         x, _ = model.predictor.lstm(d)
         duration = model.predictor.duration_proj(x)
 
         duration = torch.sigmoid(duration).sum(axis=-1)
         pred_dur = torch.round(duration.squeeze()).clamp(min=1)
-
-
         pred_aln_trg = torch.zeros(input_lengths, int(pred_dur.sum().data))
+
         c_frame = 0
         for i in range(pred_aln_trg.size(0)):
             pred_aln_trg[i, c_frame:c_frame + int(pred_dur[i].data)] = 1
             c_frame += int(pred_dur[i].data)
 
         # encode prosody
-        en = (d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(gpu_device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(en)
-            asr_new[:, :, 0] = en[:, :, 0]
-            asr_new[:, :, 1:] = en[:, :, 0:-1]
-            en = asr_new
-
+        en = get_en(d.transpose(-1, -2), pred_aln_trg, torch.float32)
         F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
-
-        asr = (t_en @ pred_aln_trg.unsqueeze(0).to(gpu_device))
-        if model_params.decoder.type == "hifigan":
-            asr_new = torch.zeros_like(asr)
-            asr_new[:, :, 0] = asr[:, :, 0]
-            asr_new[:, :, 1:] = asr[:, :, 0:-1]
-            asr = asr_new
-
-        out = model.decoder(asr,
-                                F0_pred, N_pred, ref.squeeze().unsqueeze(0))
-
-
+        asr = get_en(t_en, pred_aln_trg, USE_DTYPE)
+        out = model.decoder(asr, F0_pred, N_pred, ref.squeeze().unsqueeze(0))
     return out.squeeze().cpu().numpy()[..., :-50] # weird pulse at the end of the model, need to be fixed later
 
 
